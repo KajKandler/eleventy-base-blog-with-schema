1dfa5247ea746eb8bf6c8d35f889222c63b4d4ae
Kaj Kandler <kajkandler@yahoo.com>
2025-06-04 22:09:36 +0200

    markup all posts in a blog with article schema

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,26 @@ export default {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
+	eleventyComputed: {
+		schemaorg: {
+			"@graph": [
+				{
+				"@type": "Article",
+				"@id": '{{ page.url | constructID(metadata.url, "#article") }}',
+				name: (data) => data.title,
+				headline: (data) => data.title,
+				abstract: (data) => data.description,
+				url: '{{ page.url | constructID(metadata.url, "") }}',
+				mainEntityOfPage: {
+					"@type": "WebPage",
+					"@id": '{{ page.url | constructID(metadata.url, "#webpage") }}',
+				},
+				author: {
+					"@type": "Person",
+					"@id": '{{ "/" | constructID(metadata.url, "#person_kaj_kandler") }}'
+				},
+			}
+			]
+		}
+	}
 };
